9ccd6dd7b95aad6a2678a555d62fe124c18dba30
SergeiSSH <81880270+SergeiSSH@users.noreply.github.com>
2021-11-10 20:47:20 +0300

    Create E2.js

---
+++ E2.js
@@ -0,0 +1,27 @@
+YouN = prompt('Введиет число:');
+YouN = +YouN;
+if (typeof YouN === 'number') {
+    if (YouN != NaN){
+        if (YouN%2 == 0) {
+            console.log('Четное число');
+        }else {
+            console.log('Число нечетное');
+        }
+    }else {
+        console.log('NaN');
+    }
+}else{
+    console.log('Вы ошиблись')
+};
+
+let x = 555;
+
+if(typeof x === 'number'){
+    console.log('x - число');
+}else if(typeof x === 'string'){
+    console.log('x - строка');
+}else if(typeof x === 'boolean'){
+    console.log('x - логический тип');
+}else{
+    console.log('Тип x не определён');
+}
