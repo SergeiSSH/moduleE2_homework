777301c945717f172feeb9229c7d3f418d07f363
SergeiSSH <81880270+SergeiSSH@users.noreply.github.com>
2021-11-11 20:45:31 +0300

    Update E2.js

--- E2.js
+++ E2.js
@@ -35,3 +35,9 @@ console.log(reverseString(string))
 //з.4______________________________________________
 let rand = Math.round(Math.random(0,100)*(100))
 console.log(rand)
+//з.5______________________________________________
+let arbitrary  = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377];
+let arLen = `amount of elements:${arbitrary.length}`
+
+console.log(arLen)
+arbitrary.forEach(function(item){console.log(item);})
