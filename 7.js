cb813fa16220de43c01388947d1aac1c47d70381
SergeiSSH <81880270+SergeiSSH@users.noreply.github.com>
2021-11-12 23:26:44 +0300

    Create 7.js

---
+++ 7.js
@@ -0,0 +1,23 @@
+let somenum = [9,6,3,1,4,6,86,0,6,'q',''];
+let even = 0;
+let odd = 0;
+let zero = 0;
+
+somenum.forEach(function(item, index, array) {
+    if (typeof item === 'number') {
+        if (item === 0) {
+            zero += 1;
+        }else{
+            if (item%2 ===0){
+                even += 1;
+            }else {
+                odd += 1;
+            }
+        }
+    }else {
+        console.log(index+'№ не число')
+    }
+})
+console.log('Чётных чисел: '+even);
+console.log('Нечётных чисел: '+odd);
+console.log('Нулей: '+zero); 
