a8d1e43693c878ea2a60021186f539b03fa31b6c
SergeiSSH <81880270+SergeiSSH@users.noreply.github.com>
2021-11-12 20:01:44 +0300

    Create 5.js

---
+++ 5.js
@@ -0,0 +1,5 @@
+let arbitrary  = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377];
+let arLen = `amount of elements:${arbitrary.length}`
+
+console.log(arLen)
+arbitrary.forEach(function(item){console.log(item);})
